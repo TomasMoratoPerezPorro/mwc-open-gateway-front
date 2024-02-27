d65d43d6ebf6dc2e2fb86474c4fcc91876147670
Tomàs Morató <tomas.morato@vodafone.com>
2024-02-27 22:20:08 +0100

    feat: add dtos

--- src/types/dtos.ts
+++ src/types/dtos.ts
@@ -1,21 +1,57 @@
-export type IContact = {
-  firstName: string
-  lastName: string
-  phone: string
+export interface Festival {
+  festival_id: number;
+  festival_name: string;
 }
 
-export type UserElement = {
-  id: number
-  firstName: string
-  lastName: string
-}
-export type GetUserElementResponse = {
-  data: Array<UserElement>
+export interface Role {
+  role_id: number;
+  role_name: string;
 }
 
-export type DriverElement = {
-  id: number
-  contact: IContact
+export interface RouteStatus {
+  status_id: number;
+  status_name: string;
 }
 
-const driver: DriverElement = {contact: {firstName: 'John', lastName: 'Maverick', phone: '123456789'}, id: 1}
+export interface User {
+  user_id: number;
+  username: string;
+  password_hash?: string;
+  email: string;
+  phone_number: string;
+  role_id: number;
+  festival_id: number;
+  last_location_time?: Date;
+  last_location_coordinates?: string; // Format as 'latitude,longitude' if using POINT
+}
+
+export interface Vehicle {
+  vehicle_id: number;
+  registration_number: string;
+  rental_company: string;
+  main_driver_id: number;
+  brand: string;
+  festival_id: number;
+}
+
+export interface Route {
+  route_id: number;
+  start_coordinates_latitude: number;
+  start_coordinates_longitude: number;
+  end_coordinates_latitude: number;
+  end_coordinates_longitude: number;
+  pickup_time: Date;
+  drop_time: Date;
+  status_id: number;
+  driver_id: number;
+  vip_id: number;
+  vehicle_id: number;
+  festival_id: number;
+}
+
+export interface UserRoute {
+  user_route_id: number;
+  user_id: number;
+  route_id: number;
+  festival_id: number;
+}
