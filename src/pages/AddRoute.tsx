853eb1509094a306bb1d891acfe73c75b080c367
Melina Gamboa <melina.devel@gmail.com>
2024-02-28 14:29:09 +0100

    feat(frontend): add route modal

--- src/pages/AddRoute.tsx
+++ src/pages/AddRoute.tsx
@@ -1,20 +1,20 @@
 import { useState, useEffect } from 'react'
 import { Button, Modal, Form, DatePicker, Select } from 'antd'
 import { fetchUsersByRoleFestival } from '../services/users'
+import { fetchVehiclesByFestival } from '../services/vehicles'
+import { addRoute } from '../services/route'
 
 type FieldType = {
-  username?: string
+  user_id?: string
   startDate?: Date
-  vehicle?: string
+  vehicle_id?: string
   endDate?: Date
 }
 
 const AddRouteModal = () => {
   const [shownModal, showModal] = useState(false);
   const [usersSelect, setusersSelect] = useState<[]>([]);
-  const selectedUser = useState()
-  const vehiclesSelect = useState([])
-  const selectedVehicle = useState()
+  const [vehiclesSelect, setVehiclesSelect] = useState<[]>([]);
 
   const getUsers = async () => {
     try {
@@ -25,32 +25,35 @@ const AddRouteModal = () => {
     }
   };
 
+  const getVehicles = async () => {
+    try {
+      const {vehicles} = await fetchVehiclesByFestival();
+      setVehiclesSelect(vehicles)
+    } catch (error) {
+      if (error) return alert(error)
+    }
+  };
+
+
   useEffect(() => {
     getUsers();
+    getVehicles();
   }, []);
 
   const onFinish = async (values: FieldType) => {
     console.log('Success:', values)
-    return
-    // if (!values.username || !values.password)
-    //   return alert('Please fill all the fields')
+    if (!values.user_id || !values.startDate || !values.endDate || !values.vehicle_id)
+      return alert('Please fill all the fields')
 
-    // try {
-    //   const {
-    //     data: { user, session },
-    //     error
-    //   } = await login(values.username, values.password);
-    //   if (user && session) navigate("/");
-    // } catch (error) {
-    //   if (error) return alert(error)
-    // }
-    // login(values.username, values.password)
+    try {
+      const {
+        data,
+        error
+      } = await addRoute(values);
+    } catch (error) {
+      if (error) return alert(error)
+    }
   }
-
-  const onFinishFailed = (errorInfo: any) => {
-    alert(`Failed ${errorInfo}`)
-  }
-
 
   return (
     <>
@@ -71,7 +74,6 @@ const AddRouteModal = () => {
           style={{ maxWidth: 600 }}
           initialValues={{ remember: true }}
           onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
           autoComplete="off"
         >
           <Form.Item name="user_id" label="VIP User" rules={[{ required: true }]}>
@@ -81,8 +83,8 @@ const AddRouteModal = () => {
               allowClear
             >
               {usersSelect.map(user => (
-                <Select.Option value={user.user_id} key={user.user_id}>
-                  {user.username}                        
+                <Select.Option value={user.user_id} key={user.user_id} title={user.username} id={user.user_id}>
+                  {user.username}
                 </Select.Option>
               ))}
             </Select>
@@ -91,24 +93,29 @@ const AddRouteModal = () => {
           <Form.Item<FieldType>
             label="Start Date"
             name="startDate"
-            rules={[{ required: true, message: 'Please input your password!' }]}
+            rules={[{ required: true, message: 'Please select a date!' }]}
           >
             <DatePicker showTime format="YYYY-MM-DD HH:mm" />
           </Form.Item>
 
-          {/* <Form.Item name="vehicle" label="vehicle" rules={[{ required: true }]}>
+          <Form.Item name="vehicle_id" label="Vehicle" rules={[{ required: true }]}>
             <Select
+            showSearch={true}
               placeholder="Select a vehicle"
               allowClear
             >
-              <Option value=""></Option>
+              {vehiclesSelect.map(vehicle => (
+                <Select.Option value={vehicle.vehicle_id} key={vehicle.vehicle_id}>
+                  {vehicle.name} - {vehicle.brand}
+                </Select.Option>
+              ))}
             </Select>
-          </Form.Item> */}
+          </Form.Item>
 
           <Form.Item<FieldType>
             label="End Date"
             name="endDate"
-            rules={[{ required: true, message: 'Please input your password!' }]}
+            rules={[{ required: true, message: 'Please select a date!' }]}
           >
             <DatePicker showTime format="YYYY-MM-DD HH:mm" />
           </Form.Item>
